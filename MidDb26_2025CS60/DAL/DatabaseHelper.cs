c32bf2f50cc9fac9f16f5a85ea07f61474c72a3d
muhammadumerfareed <theumerfareed@gmail.com>
2026-03-12 00:09:10 +0500

    Day 1: Database setup and DatabaseHelper class

---
+++ MidDb26_2025CS60/DAL/DatabaseHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+namespace MidDb26_2025CS60.DAL
+{
+    public class DatabaseHelper
+    {
+        
+        private static string connectionString ="Server=localhost;Port=3306;Database=projectadb26;Uid=root;Pwd=m55433001@Umer;";
+       
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
